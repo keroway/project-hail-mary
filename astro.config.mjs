e12ff8b8cebaa7cd8f6ef68bf4af9db55ff9cff9
keroway <4470654+keroway@users.noreply.github.com>
2026-07-11 02:49:33 +0900

    Upgrade: Astro 6 to 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://hailmary.keroway.com",
   output: "static",
+  compressHTML: true,
   build: {
     assets: "_astro",
   },
